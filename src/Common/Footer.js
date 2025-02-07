653b61c134f4fbe686b72bdb4ee87bd182659a66
dp <codewithdp2073@gmail.com>
2025-02-07 17:11:31 +0530

    ContactPage ready

--- src/Common/Footer.js
+++ src/Common/Footer.js
@@ -3,46 +3,46 @@ import React from 'react'
 const Footer = () => {
     return (
         <div>
-            <section class="w3l-market-footer">
-                <footer class="footer-28 pt-5">
-                    <div class="footer-bg-layer">
-                        <div class="container py-lg-3">
-                            <div class="row footer-top-28">
-                                <div class="col-lg-6 footer-list-28">
-                                    <h2 class="footer-title-28"><a href="index.html"> Estate Property </a></h2>
-                                    <p class="mt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+            <section className="w3l-market-footer">
+                <footer className="footer-28 pt-5 px-5">
+                    <div className="footer-bg-layer">
+                        <div className="container py-lg-3">
+                            <div className="row footer-top-28">
+                                <div className="col-lg-6 footer-list-28">
+                                    <h2 className="footer-title-28"><a href="index.html"> Estate Property </a></h2>
+                                    <p className="mt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
                                         incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
                                         ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                                    <div class="main-social-footer-28 mt-3">
-                                        <ul class="social-icons">
-                                            <li class="facebook">
+                                    <div className="main-social-footer-28 mt-3">
+                                        <ul className="social-icons">
+                                            <li className="facebook">
                                                 <a href="#link" title="Facebook">
-                                                    <span class="fa fa-facebook" aria-hidden="true"></span>
+                                                    <span className="fa fa-facebook" aria-hidden="true"></span>
                                                 </a>
                                             </li>
-                                            <li class="twitter">
+                                            <li className="twitter">
                                                 <a href="#link" title="Twitter">
-                                                    <span class="fa fa-twitter" aria-hidden="true"></span>
+                                                    <span className="fa fa-twitter" aria-hidden="true"></span>
                                                 </a>
                                             </li>
-                                            <li class="dribbble">
+                                            <li className="dribbble">
                                                 <a href="#link" title="Dribbble">
-                                                    <span class="fa fa-dribbble" aria-hidden="true"></span>
+                                                    <span className="fa fa-dribbble" aria-hidden="true"></span>
                                                 </a>
                                             </li>
-                                            <li class="google">
+                                            <li className="google">
                                                 <a href="#link" title="Google">
-                                                    <span class="fa fa-google" aria-hidden="true"></span>
+                                                    <span className="fa fa-google" aria-hidden="true"></span>
                                                 </a>
                                             </li>
                                         </ul>
                                     </div>
                                 </div>
-                                <div class="col-lg-6 mt-lg-0 mt-4">
-                                    <div class="row">
-                                        <div class="col-md-4 footer-list-28">
-                                            <h6 class="footer-title-28">Important Links</h6>
-                                            <ul class="mt-4">
+                                <div className="col-lg-6 mt-lg-0 mt-4">
+                                    <div className="row">
+                                        <div className="col-md-4 footer-list-28">
+                                            <h6 className="footer-title-28">Important Links</h6>
+                                            <ul className="mt-4">
                                                 <li><a href="index.html">Home</a></li>
                                                 <li><a href="about.html">About</a></li>
                                                 <li><a href="services.html">Services</a></li>
@@ -51,25 +51,25 @@ const Footer = () => {
                                             </ul>
                                         </div>
 
-                                        <div class="col-md-8 footer-list-28 mt-md-0 mt-4">
-                                            <h6 class="footer-title-28">Recent Posts</h6>
-                                            <div class="fot-top row mt-4">
-                                                <div class="col-4">
+                                        <div className="col-md-8 footer-list-28 mt-md-0 mt-4">
+                                            <h6 className="footer-title-28">Recent Posts</h6>
+                                            <div className="fot-top row mt-4">
+                                                <div className="col-4">
                                                     <a href="blog-single.html">
-                                                        <img src="assets/images/g1.jpg" class="img-responsive" alt="" /></a>
+                                                        <img src="assets/images/g1.jpg" className="img-responsive" alt="" /></a>
                                                 </div>
-                                                <div class="col-8 pl-2">
+                                                <div className="col-8 pl-2">
                                                     <h5><a href="blog-single.html">We Are Leading International Consultiing Agency</a></h5>
-                                                    <p><span class="fa fa-clock-o"></span> December 17, 2019</p>
+                                                    <p><span className="fa fa-clock-o"></span> December 17, 2019</p>
                                                 </div>
                                             </div>
-                                            <div class="fot-top row mt-4">
-                                                <div class="col-4">
-                                                    <a href="blog-single.html"><img src="assets/images/g2.jpg" class="img-responsive" alt="" /></a>
+                                            <div className="fot-top row mt-4">
+                                                <div className="col-4">
+                                                    <a href="blog-single.html"><img src="assets/images/g2.jpg" className="img-responsive" alt="" /></a>
                                                 </div>
-                                                <div class="col-8 pl-2">
+                                                <div className="col-8 pl-2">
                                                     <h5> <a href="blog-single.html">We Are Leading International Consultiing Agency</a></h5>
-                                                    <p> <span class="fa fa-clock-o"></span> December 27, 2019</p>
+                                                    <p> <span className="fa fa-clock-o"></span> December 27, 2019</p>
                                                 </div>
                                             </div>
 
@@ -78,9 +78,9 @@ const Footer = () => {
                                 </div>
                             </div>
                         </div>
-                        <div class="midd-footer-28 text-center py-lg-4 py-3 mt-5">
-                            <div class="container">
-                                <p class="copy-footer-28 text-center"> &copy; 2025 Estate Property. All Rights Reserved
+                        <div className="midd-footer-28 text-center py-lg-4 py-3 mt-5">
+                            <div className="container">
+                                <p className="copy-footer-28 text-center"> &copy; 2025 Estate Property. All Rights Reserved
                                 </p>
                             </div>
                         </div>
