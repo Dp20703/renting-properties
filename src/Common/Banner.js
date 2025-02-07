f4d40dd2a80c1a4bc8e14ba6743903ada93ad154
dp <codewithdp2073@gmail.com>
2025-02-07 18:16:00 +0530

    Banner Ready

---
+++ src/Common/Banner.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Banner = () => {
+    return (
+        <div>
+            <section class="w3l-inner-banner-main">
+                <div class="about-inner">
+                    <div class="container">
+                        <ul class="breadcrumbs-custom-path">
+                            <h3>About</h3>
+                            <li><a href="index.html"><span class="fa fa-home" aria-hidden="true"></span> <span
+                                class="fa fa-angle-double-right" aria-hidden="true"></span></a></li>
+                            <li class="active">About</li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Banner
